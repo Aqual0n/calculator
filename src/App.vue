a12cafc8b418fdef4b4955d8aee0a0bc887f37de
Vyacheslav Lashkin <vl@burno.tech>
2020-01-17 17:38:59 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template lang="pug">
+    include tools/mixins.pug
+    #app
+        main-component
+
+</template>
+
+<script>
+  import Main from "./components/Main.vue";
+  export default {
+      data() {
+          return {
+
+          }
+      },
+      components: {
+          'main-component': Main,
+      }
+  }
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,88 @@
+<template lang="pug">
+    include ../tools/mixins.pug
+
+    +b.SECTION.main
+        +e.container
+            +e.H1.title Калькулятор
+
+            +e.label Введите делитель в поле ниже
+            +e.INPUT.input--divider(
+                v-model="divider"
+            )
+            +e.label Введите делимое число в поле ниже
+            +e.INPUT.input--number(
+                v-model="number"
+            )
+
+            +e.result(
+                :class="{'wrong': !dividable}"
+            ) Число {{number}} {{getResult}} без остатка на {{divider}}
+
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                resultPresent: false,
+                result: null,
+                number: 96,
+                divider: 8,
+                wrong: false,
+            }
+        },
+        methods: {
+        },
+        computed: {
+            getResult() {
+                if (this.dividable) {
+                    return 'делится';
+                } else {
+                    return 'не делится';
+                }
+            },
+            dividable() {
+                if(this.number%this.divider === 0) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        }
+    }
+</script>
+<style scoped lang="scss">
+.main {
+    &__container {
+        max-width: 1200px;
+        margin: 0 auto;
+        text-align: center;
+    }
+
+    &__input {
+        display: block;
+        width: 300px;
+        margin: 0 auto;
+        border-radius: 5px;
+        border: 1px solid lightgray;
+    }
+
+    &__label {
+        margin: 16px 0;
+    }
+
+    &__button {
+        margin-top: 16px;
+    }
+
+    &__result {
+        font-size: 32px;
+        line-height: 48px;
+        margin-top: 16px;
+
+        &.wrong {
+            color: red;
+        }
+    }
+}
+</style>
