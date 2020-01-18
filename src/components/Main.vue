a06c7918e5303ac0633faf9d4cef7f012ce0b1cc
Vyacheslav Lashkin <vl@burno.tech>
2020-01-18 11:37:50 +0300

    added closest variants

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -5,18 +5,72 @@
         +e.container
             +e.H1.title Калькулятор
 
-            +e.label Введите делитель в поле ниже
-            +e.INPUT.input--divider(
-                v-model="divider"
-            )
-            +e.label Введите делимое число в поле ниже
-            +e.INPUT.input--number(
-                v-model="number"
-            )
+            +e.label Выберите делитель:
+
+            +e.wrapper
+                +e.fieldset
+                    +e.item
+                        +e.LABEL.radio-label(
+                            for="four"
+                        ) 4
+                        +e.INPUT.radio(
+                            type="radio"
+                            id="four"
+                            value="4"
+                            v-model="divider"
+                        )
+                    +e.item
+                        +e.LABEL.radio-label(
+                            for="eight"
+                        ) 8
+                        +e.INPUT.radio(
+                            type="radio"
+                            id="eight"
+                            value="8"
+                            v-model="divider"
+                        )
+                    +e.item
+                        +e.LABEL.radio-label(
+                            for="ten"
+                        ) 10
+                        +e.INPUT.radio(
+                            type="radio"
+                            id="ten"
+                            value="10"
+                            v-model="divider"
+                        )
+                    +e.item
+                        +e.LABEL.radio-label(
+                            for="twelve"
+                        ) 12
+                        +e.INPUT.radio(
+                            type="radio"
+                            id="twelve"
+                            value="12"
+                            v-model="divider"
+                        )
+                +e.BUTTON.input--button(
+                    v-on:click="toggleInputMode"
+                    v-if="!customInput"
+                ) ...или введите вручную
+                +e.INPUT.input--divider(
+                    v-model="divider"
+                    v-if="customInput"
+                    v-on:blur="toggleInputMode"
+                    ref="dividerInput"
+                )
+                +e.label Введите число:
+                +e.INPUT.input--number(
+                    v-model="number"
+                )
 
             +e.result(
                 :class="{'wrong': !dividable}"
-            ) Число {{number}} {{getResult}} без остатка на {{divider}}
+            ) {{getResult}}
+
+            +e.closest(
+                v-if="!dividable"
+            ) Ближайшие числа, которые делятся - {{getClosest.closestMin}} и {{getClosest.closestMax}}
 
 </template>
 
@@ -29,9 +83,18 @@
                 number: 96,
                 divider: 8,
                 wrong: false,
+                customInput: false,
             }
         },
         methods: {
+            toggleInputMode() {
+                this.customInput = !this.customInput;
+                if(this.customInput === true) {
+                    this.$nextTick(()=> {
+                        this.$refs.dividerInput.focus();
+                    });
+                }
+            },
         },
         computed: {
             getResult() {
@@ -47,6 +110,17 @@
                 } else {
                     return false;
                 }
+            },
+            getClosest() {
+                if(!this.dividable) {
+                    let closestMin = this.number - this.number%this.divider;
+                    let closestMax = +this.number + +this.divider - this.number%this.divider;
+
+                    return {
+                        closestMin: closestMin,
+                        closestMax: closestMax,
+                    }
+                }
             }
         }
     }
@@ -59,20 +133,38 @@
         text-align: center;
     }
 
+    &__wrapper {
+        display: inline-block;
+
+        width: 300px;
+    }
+
     &__input {
         display: block;
-        width: 300px;
         margin: 0 auto;
-        border-radius: 5px;
+        border-radius: 10px;
         border: 1px solid lightgray;
+
+        padding: 8px 16px;
+        box-sizing: border-box;
+
+        width: 100%;
+        font-size: 16px;
+        line-height: 21px;
+
+        &--button {
+            background-color: #fff;
+            transition: .3s ease;
+            transition-property: border-color, background-color;
+            &:hover {
+                border-color: transparent;
+                background-color: lightblue;
+            }
+        }
     }
 
     &__label {
-        margin: 16px 0;
-    }
-
-    &__button {
-        margin-top: 16px;
+        margin: 8px 0;
     }
 
     &__result {
@@ -82,6 +174,17 @@
 
         &.wrong {
             color: red;
+        }
+    }
+
+    &__fieldset {
+        margin-bottom: 8px;
+    }
+
+    &__item {
+        display: inline-block;
+        & + & {
+            margin-left: 8px;
         }
     }
 }
